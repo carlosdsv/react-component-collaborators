147ee35d59ada505a081872dadb052c5f54cf664
Carlos De Sousa <carlosdsv@gmail.com>
2020-11-16 23:39:28 +0100

    creates Demo app for library

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,381 @@
-import React from 'react'
+import React, { ChangeEventHandler, MouseEventHandler} from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { CollaboratorsList} from 'react-component-collaborators';
+import { CardComplete } from 'react-component-collaborators';
+import { CardMinimal } from 'react-component-collaborators';
+import collaboratorsArray from './mock/collaboratorsArray';
+import { borderI, cardStyleObjectI, listStyleObjectI } from './interfaces';
 
-import { ExampleComponent } from 'react-component-collaborators'
-import 'react-component-collaborators/dist/index.css'
+const AppStyle = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-around;
+`;
+const CustomizationDiv = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+`;
+const SizeInput = styled.input`
+height: 20px;
+width: 25%
+`;
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+const App:React.FC = () => {
+  // Card component
+  const [cardComponent, setCardComponent] = useState<string>('complete');
+  const [imagePosition, setImagePosition] = useState<string>('left');
+
+  // List state
+  const [listStyleObject, setListStyleObject] = useState<listStyleObjectI>({
+    listWidth: '700px',
+    listHeight: '400px', 
+    direction: 'vertical',
+    listBackgroundColor: '#FFF'
+  });
+
+  // Card state
+  const [borderImage, setBorderImage] = useState<borderI>({
+    width: '4px',
+    style: 'solid',
+    color: '#E8C3C9'
+  });
+  const [borderCard, setBorderCard] = useState<borderI>({
+    width: '4px',
+    style: 'solid',
+    color: '#E8C3C9'
+  });
+  const [cardStyleObject, setCardStyleObject] = useState<cardStyleObjectI>({
+    cardWidth: '300px', 
+    cardHeight: '150px',
+    cardBackgroundColor: '#7EE8FA',
+    textAlign: 'center',
+    imageFormat: 'circle',
+    borderImage: borderImage,
+    borderCard: borderCard,
+    titleColor: '#000',
+    descriptionColor: '#000',
+    imageWidth: '100px',
+    imageHeight: '100px',
+    fontSizeTitle: '100%',
+    paddingTitle: '0px 0px 20px 0px',
+    marginTitle: '0px 0px 0px 0px',
+    fontSizeDescription: '80%',
+    paddingDescription: '0px 0px 0px 0px',
+    marginDescription: '0px 0px 0px 0px'
+  });
+
+  // Code snippet
+  const collaborarorsArraySnippet:string = `
+  [{
+    image: 'mandatory url',
+    title: 'mandatory title',
+    description: 'optional description'
+  }, {}, ...]
+  `;
+  const basicStructureSnippet:string = `
+  <CollaboratorsList collaboratorsArray=[collaboratorsArray] listStyleObject={listStyleObject}>
+    ${cardComponent === 'complete'? '<CardComplete': '<CardMinimal'} cardStyleObject={cardStyleObject} imagePosition={imagePosition}/>
+  </CollaboratorsList>
+  `;
+  const codeSnippet:string = `
+  <CollaboratorsList
+    collaboratorsArray= Insert your array here!
+    listStyleObject={{
+      listWidth: ${listStyleObject.listWidth},
+      listHeight: ${listStyleObject.listHeight}, 
+      direction: ${listStyleObject.direction},
+      listBackgroundColor: ${listStyleObject.listBackgroundColor}
+    }}>
+    ${cardComponent === 'complete'? '<CardComplete': '<CardMinimal'}
+    imagePosition=${imagePosition}
+    cardStyleObject={
+      cardWidth: ${cardStyleObject.cardWidth},
+      cardHeight: ${cardStyleObject.cardHeight},
+      cardBackgroundColor: ${cardStyleObject.cardBackgroundColor},
+      textAlign: ${cardStyleObject.textAlign},
+      imageFormat: ${cardStyleObject.imageFormat},
+      borderImage: {
+        width: ${cardStyleObject.borderImage.width},
+        style: ${cardStyleObject.borderImage.style},
+        color: ${cardStyleObject.borderImage.color}
+      },
+      borderCard: {
+        width: ${cardStyleObject.borderCard.width},
+        style: ${cardStyleObject.borderCard.style},
+        color: ${cardStyleObject.borderCard.color}
+      },
+      titleColor: ${cardStyleObject.titleColor},
+      descriptionColor: ${cardStyleObject.descriptionColor},
+      imageWidth: ${cardStyleObject.imageWidth},
+      imageHeight: ${cardStyleObject.imageHeight},
+      fontSizeTitle: ${cardStyleObject.fontSizeTitle},
+      paddingTitle: ${cardStyleObject.paddingTitle},
+      marginTitle: ${cardStyleObject.marginTitle},
+      fontSizeDescription: ${cardStyleObject.fontSizeDescription},
+      paddingDescription: ${cardStyleObject.paddingDescription},
+      marginDescription: ${cardStyleObject.marginDescription}
+    } />}
+  </CollaboratorsList>
+  `;
+  
+  // Type of card functions
+  const handleTypeOfCardComplete: MouseEventHandler<HTMLButtonElement> = () => {
+    setCardComponent('complete')
+  }
+  const handleTypeOfCardMinimal: MouseEventHandler<HTMLButtonElement> = () => {
+    setCardComponent('minimal')
+  }
+  const handleImagePosition: MouseEventHandler<HTMLButtonElement> = (e) => {
+    setImagePosition(e.currentTarget.value)
+  }
+
+  // List functions
+  const handleListWidth: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setListStyleObject({...listStyleObject, listWidth: e.target.value})
+  }
+  const handleListHeight: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setListStyleObject({...listStyleObject, listHeight: e.target.value})
+  }
+  const handleListBackgroundColor: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setListStyleObject({...listStyleObject, listBackgroundColor: e.target.value})
+  }
+  const handleDirection: MouseEventHandler<HTMLButtonElement> = (e) => {
+    setListStyleObject({...listStyleObject, direction: e.currentTarget.value})
+  }
+
+  // Card functions
+  const handleCardWidth: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, cardWidth: e.target.value})
+  }
+  const handleCardHeight: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, cardHeight: e.target.value})
+  }
+  const handleTextAlign: MouseEventHandler<HTMLButtonElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, textAlign: e.currentTarget.value})
+  }
+  const handleCardBackgroundColor: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, cardBackgroundColor: e.target.value})
+  }
+  const handleImageFormat: MouseEventHandler<HTMLButtonElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, imageFormat: e.currentTarget.value})
+  }
+  const handleTitleColor: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, titleColor: e.currentTarget.value})
+  }
+  const handleDescriptionColor: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, descriptionColor: e.currentTarget.value})
+  }
+  const handleImageWidth: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, imageWidth: e.currentTarget.value})
+  }
+  const handleImageHeight: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, imageHeight: e.currentTarget.value})
+  }
+  const handleFontSizeTitle: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, fontSizeTitle: e.currentTarget.value})
+  }
+  const handleMarginTitle: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, marginTitle: e.currentTarget.value})
+  }
+  const handlePaddingTitle: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, paddingTitle: e.currentTarget.value})
+  }
+  const handleFontSizeDescription: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, fontSizeDescription: e.currentTarget.value})
+  }
+  const handleMarginDescription: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, marginDescription: e.currentTarget.value})
+  }
+  const handlePaddingDescription: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCardStyleObject({...cardStyleObject, paddingDescription: e.currentTarget.value})
+  }
+  const handleBorderCardWidth: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setBorderCard({...borderCard, width: e.currentTarget.value})
+    setCardStyleObject({...cardStyleObject, borderCard})
+  }
+  const handleBorderCardStyle: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setBorderCard({...borderCard, style: e.currentTarget.value})
+    setCardStyleObject({...cardStyleObject, borderCard})
+  }
+  const handleBorderCardColor: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setBorderCard({...borderCard, color: e.currentTarget.value})
+    setCardStyleObject({...cardStyleObject, borderCard})
+  }
+  const handleBorderImageWidth: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setBorderImage({...borderImage, width: e.currentTarget.value})
+    setCardStyleObject({...cardStyleObject, borderImage})
+  }
+  const handleBorderImageStyle: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setBorderImage({...borderImage, style: e.currentTarget.value})
+    setCardStyleObject({...cardStyleObject, borderImage})
+  }
+  const handleBorderImageColor: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setBorderImage({...borderImage, color: e.currentTarget.value})
+    setCardStyleObject({...cardStyleObject, borderImage})
+  }
+
+  return (
+    <div>
+    <h1>React Customizable Collaborators Component</h1>
+    <AppStyle>
+      <CollaboratorsList collaboratorsArray={collaboratorsArray} listStyleObject={listStyleObject}>
+        {cardComponent === 'complete' ? <CardComplete cardStyleObject={cardStyleObject} imagePosition={imagePosition}/>: 
+        cardComponent === 'minimal' ? <CardMinimal cardStyleObject={cardStyleObject} imagePosition={imagePosition}/>:
+        <CardComplete cardStyleObject={cardStyleObject} imagePosition={imagePosition}/>}
+      </CollaboratorsList>
+    </AppStyle>
+    <div>
+      <p>Type of Card:</p>
+      <button type="button" onClick={handleTypeOfCardComplete}>Complete</button>
+      <button type="button" onClick={handleTypeOfCardMinimal}>Minimal</button>
+      <p>Image position:</p>
+      <button type="button" value='left' onClick={handleImagePosition}>Left</button>
+      <button type="button" value='right'onClick={handleImagePosition}>Right</button>
+      <button type="button" value='top' onClick={handleImagePosition}>Top</button>
+      <button type="button" value='bottom' onClick={handleImagePosition}>Bottom</button>
+      <CustomizationDiv>
+        <div>
+          <p>Properties for the container:</p>
+          <div>
+            <p >Width</p>
+            <SizeInput placeholder={listStyleObject.listWidth} onChange={handleListWidth}/>
+          </div>
+          <div>
+            <p>Height</p>
+            <SizeInput type="text" placeholder={listStyleObject.listHeight} onChange={handleListHeight}/>
+          </div>
+          <div>
+            <p>Display direction</p>
+            <button type="button" value='vertical' onClick={handleDirection}>Vertical</button>
+            <button type="button" value='horizontal' onClick={handleDirection}>Horizontal</button>
+            <button type="button" value='none' onClick={handleDirection}>None</button>
+          </div>
+          <div>
+            <p>Background color:</p>
+            <input type='text' placeholder={listStyleObject.listBackgroundColor} onChange={handleListBackgroundColor}/>
+            <input type='color' value={listStyleObject.listBackgroundColor} placeholder={listStyleObject.listBackgroundColor} onChange={handleListBackgroundColor}/>
+          </div>
+        </div>
+
+        <div>
+
+          <p>Properties for the card:</p>
+          <div>
+            <p>Width</p>
+            <SizeInput type="text" placeholder={cardStyleObject.cardWidth} onChange={handleCardWidth}/>
+          </div>
+          <div>
+            <p>Height</p>
+            <SizeInput type="text" placeholder={cardStyleObject.cardHeight} onChange={handleCardHeight}/>
+          </div>
+          <div>
+            <p>Text align</p>
+            <button type="button" value='left' onClick={handleTextAlign}>Left</button>
+            <button type="button" value='center' onClick={handleTextAlign}>Center</button>
+            <button type="button" value='right' onClick={handleTextAlign}>Right</button>
+          </div>
+          <div>
+            <p>Background color:</p>
+            <input type='text' placeholder={cardStyleObject.cardBackgroundColor} onChange={handleCardBackgroundColor}/>
+            <input type='color' value={cardStyleObject.cardBackgroundColor} onChange={handleCardBackgroundColor}/>
+          </div>
+          <div>
+            <p>Image format:</p>
+            <button type="button" value='circle' onClick={handleImageFormat}>circle</button>
+            <button type="button" value='square' onClick={handleImageFormat}>Square</button>
+          </div>
+          <div>
+            <p>Title color:</p>
+            <input type='text' placeholder={cardStyleObject.titleColor} onChange={handleTitleColor}/>
+            <input type='color' value={cardStyleObject.titleColor} onChange={handleTitleColor}/>
+          </div>
+          {cardComponent === 'complete' ? (
+            <div>
+              <p>Description color:</p>
+              <input type='text' placeholder={cardStyleObject.descriptionColor} onChange={handleDescriptionColor}/>
+              <input type='color' value={cardStyleObject.descriptionColor} onChange={handleDescriptionColor}/>
+            </div>
+          ): <></>}
+          <div>
+            <p>Image width</p>
+            <SizeInput type="text" placeholder={cardStyleObject.imageWidth} onChange={handleImageWidth}/>
+          </div>
+          <div>
+            <p>Image height</p>
+            <SizeInput type="text" placeholder={cardStyleObject.imageHeight} onChange={handleImageHeight}/>
+          </div>
+          <div>
+            <p>Title font size</p>
+            <SizeInput type="text" placeholder={cardStyleObject.fontSizeTitle} onChange={handleFontSizeTitle}/>
+          </div>
+          <div>
+            <p>Title padding</p>
+            <input type="text" placeholder={cardStyleObject.marginTitle} onChange={handleMarginTitle}/>
+          </div>
+          <div>
+            <p>Title margin</p>
+            <input type="text" placeholder={cardStyleObject.paddingTitle} onChange={handlePaddingTitle}/>
+          </div>
+          <div>
+            <p>Description font size</p>
+            <SizeInput type="text" placeholder={cardStyleObject.fontSizeDescription} onChange={handleFontSizeDescription}/>
+          </div>
+          <div>
+            <p>Description padding</p>
+            <input type="text" placeholder={cardStyleObject.marginDescription} onChange={handleMarginDescription}/>
+          </div>
+          <div>
+            <p>Description margin</p>
+            <input type="text" placeholder={cardStyleObject.paddingDescription} onChange={handlePaddingDescription}/>
+          </div>
+          <div>
+            <p>Card border width</p>
+            <SizeInput type="text" placeholder={cardStyleObject.borderCard.width} onChange={handleBorderCardWidth}/>
+          </div>
+          <div>
+            <p>Card border style</p>
+            <input type="text" placeholder={cardStyleObject.borderCard.style} onChange={handleBorderCardStyle}/>
+          </div>
+          <div>
+            <p>Card border color</p>
+            <input type="text" placeholder={cardStyleObject.borderCard.color} onChange={handleBorderCardColor}/>
+            <input type='color' value={cardStyleObject.borderCard.color} onChange={handleBorderCardColor}/>
+          </div>
+          <div>
+            <p>Image border width</p>
+            <SizeInput type="text" placeholder={cardStyleObject.borderImage.width} onChange={handleBorderImageWidth}/>
+          </div>
+          <div>
+            <p>Image border style</p>
+            <input type="text" placeholder={cardStyleObject.borderImage.style} onChange={handleBorderImageStyle}/>
+          </div>
+          <div>
+            <p>Image border color</p>
+            <input type="text" placeholder={cardStyleObject.borderImage.color} onChange={handleBorderImageColor}/>
+            <input type='color' value={cardStyleObject.borderImage.color} onChange={handleBorderImageColor}/>
+          </div>
+        </div>
+      </CustomizationDiv>
+      {/* Code snippet */}
+      <div>
+        <p>Basic structure</p>
+        <pre>
+          <code>{basicStructureSnippet}</code>
+        </pre>
+        <p>Array structure</p>
+        <pre>
+          <code>{collaborarorsArraySnippet}</code>
+        </pre>
+        <p>Code snippet</p>
+        <pre>
+          <code>{codeSnippet}</code>
+        </pre>
+      </div>
+    </div>
+    </div>
+  )
 }
-
+  
 export default App
---
+++ example/src/mock/collaboratorsArray.tsx
@@ -0,0 +1,56 @@
+import { collaborator } from '../interfaces';
+
+const collaboratorsArray: collaborator[] = [
+  {
+    image: 'https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&auto=format&fit=crop&w=639&q=80',
+    title: 'Daenerys Targaryen',
+    description: 'Learning to howl'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    title: 'Jhon Snow',
+    description: 'Loves playing catch'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1583511655826-05700d52f4d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+    title: 'Arya Stark',
+    description: 'Has a nice wardrobe'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80',
+    title: 'Tyriono Lannister',
+    description: 'Feels superior'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&auto=format&fit=crop&w=660&q=80',
+    title: 'Khal Drogo',
+    description: 'Scared of hes own shadow'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixlib=rb-1.2.1&auto=format&fit=crop&w=564&q=80',
+    title: 'Joffrey Baratheon',
+    description: 'Loves smelling the flowers'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+    title: 'Theon Greyjoy',
+    description: 'Loves going for walks in the park'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1559190394-df5a28aab5c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80',
+    title: 'Ramsay Bolton',
+    description: 'Enjoys the wind while riding the car'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1537151769678-eb003fb3a153?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80',
+    title: 'Gregor Clegane',
+    description: 'Loves taking naps'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1558788353-f76d92427f16?ixlib=rb-1.2.1&auto=format&fit=crop&w=678&q=80',
+    title: 'Petyr Baelish',
+    description: 'Smiles a lot'
+  }
+];
+
+export default collaboratorsArray;
