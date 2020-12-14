7c29256b8501e4ba202af54e499d72b40f86eab5
Carlos <carlosdsv@gmail.com>
2020-12-14 14:30:14 +0100

    Updates

---
+++ precache-manifest.f2de735443d70cf9457d67023116ba0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8beb72303ac4fb2f8bf8e3a469d9605d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46e55a7b52064af0f201",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "613f5cc02b60c31497bd",
+    "url": "./static/js/2.a73c9b0f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.a73c9b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e55a7b52064af0f201",
+    "url": "./static/js/main.5d2e355c.chunk.js"
+  },
+  {
+    "revision": "ed1ec9c648a0ec2c704e",
+    "url": "./static/js/runtime-main.6b375c77.js"
+  }
+]);
